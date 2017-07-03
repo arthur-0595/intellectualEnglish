b43ee01c7ba6b53fa50b4b5c46e3567ac186bd93
尹彦 <751410306@qq.com>
2017-07-03 18:33:16 +0800

    登陆，首页以及选择版本获取系列等

--- js/alternativeVersion.js
+++ js/alternativeVersion.js
@@ -1,5 +1,59 @@
 $(function(){
+	//获取用户名
+	var userMessage = sessionStorage.userMessage;
+	userMessage = JSON.parse(userMessage);
+	var username = userMessage[0].ID;
 	
+	//切换在学课程及全部课程
+	$("#courseNavs>li").on("click",function(){
+		var thisindex = $(this).index() ;
+		$("#courseNavs>li").removeClass("checked");
+		$(this).addClass("checked");
+		
+		$("#mainBot>li").hide();
+		$("#mainBot>li").eq(thisindex).show();
+		
+		if(thisindex == 0){
+			$.ajax({
+				type:"POST",
+				url:thisUrl + '/Areas/Api/Interface.ashx',
+				dataType: "json",
+				data:{
+					method:"GetSeries",
+					user_id:username
+				},
+				success:function(data){
+					alert(JSON.stringify(data) );
+				},
+				error:function(){
+					
+				}
+			});
+		}else{
+			$.ajax({
+				type:"POST",
+				url:thisUrl + '/Areas/Api/Interface.ashx',
+				dataType: "json",
+				data:{
+					method:"GetSeries",
+					user_id:username
+				},
+				success:function(data){
+//					alert(JSON.stringify(data) );
+					var mainleft = document.getElementById('mainleft');
+					var template1 = document.getElementById('template1');
+//					alert(template1.innerHTML);
+					
+					fnupdateDoT(data[0] , mainleft , template1 );
+				},
+				error:function(){
+					
+				}
+			});
+		}
+	})
+	
+	//选择教材
 	$("#course").on("click",function(){
 		$("#selectingTextbooks").toggle();
 		if($("#selectingTextbooks").css("display") == "block"){
@@ -9,8 +63,9 @@ $(function(){
 			$("#course").css("border-radius","18px");
 			$("#course").find("i").css("transform","rotate(0deg)");
 		}
-		
 	})
+	
+	//选择章节
 	$("#sectionBox").on("click",function(){
 		$("#chapterList").toggle();
 		if($("#chapterList").css("display") == "block"){
@@ -21,6 +76,8 @@ $(function(){
 			$("#sectionBox").find("i").css("transform","rotate(0deg)");
 		}
 	})
+	
+	//测试相关
 	$("#test").on("click",function(){
 		$("#testList").toggle();
 		if($("#testList").css("display") == "block"){
@@ -31,4 +88,5 @@ $(function(){
 			$("#test").find("i").css("transform","rotate(0deg)");
 		}
 	})
+	
 })
--- js/common.js
+++ js/common.js
@@ -1,7 +1,19 @@
+//本地地址
+var thisUrl = 'http://192.168.2.127:8090';
 
+var thisUrl2 = 'http://192.168.2.111:8015';
+//服务器地址
+var serverUrl = '';
 
-
-
+//dot模版函数
+function fnupdateDoT(data_ , boxId , temId ) {
+	// 1. 编译模板函数
+	var tempFn = doT.template( temId.innerHTML ) ;
+	// 2. 多次使用模板函数
+	var resultText = tempFn(data_);
+	boxId.innerHTML = resultText;
+	alert(resultText);
+}
 
 
 
