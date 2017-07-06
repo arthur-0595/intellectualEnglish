c0b1b11687d243340ee9d974772c0b862c507a1b
尹彦 <751410306@qq.com>
2017-07-06 13:37:21 +0800

    单词记忆-记忆强化-测试（未完成）

--- js/word_memory.js
+++ js/word_memory.js
@@ -1,13 +1,13 @@
 $(function() {
 	//获取用户ID
 	var userMessage = sessionStorage.userMessage;
-		if(userMessage) {
-	userMessage = JSON.parse(userMessage);
-	var username = userMessage[0].ID;
-		} else {
-			alert('检测到您未登录，请先登录！');
-			window.location = '../index.html';
-		}
+	if(userMessage) {
+		userMessage = JSON.parse(userMessage);
+		var username = userMessage[0].ID;
+	} else {
+		alert('检测到您未登录，请先登录！');
+		window.location = '../index.html';
+	}
 	//当前选择的版本ID，教材ID ,选择的章节
 	var textbook_id, version_id, chapter_id;
 	var textbook_name, version_name, chapter_name;
@@ -19,23 +19,25 @@ $(function() {
 	var audioplaySrc;
 	//当前题目ID
 	var thiswordId;
+	//两次》一次》跳转
+	var num = 2;
 
-//	$.getUrlParam = function(name) {
-//		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-//		var r = window.location.search.substr(1).match(reg);
-//		if(r != null) return decodeURI(r[2]);
-//		return null;
-//	};
-	textbook_id = sessionStorage.textbook_id ;
-	version_id = sessionStorage.version_id ;
-	chapter_id = sessionStorage.chapter_id ;
-	typeStr = sessionStorage.typeStr ;
-	textbook_name = sessionStorage.textbook_name ;
-	version_name = sessionStorage.version_name ;
-	chapter_name = sessionStorage.chapter_name ;
-	type = sessionStorage.type ;
-	typeEnglish = sessionStorage.typeEnglish ;
-	
+	//	$.getUrlParam = function(name) {
+	//		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	//		var r = window.location.search.substr(1).match(reg);
+	//		if(r != null) return decodeURI(r[2]);
+	//		return null;
+	//	};
+	textbook_id = sessionStorage.textbook_id;
+	version_id = sessionStorage.version_id;
+	chapter_id = sessionStorage.chapter_id;
+	typeStr = sessionStorage.typeStr;
+	textbook_name = sessionStorage.textbook_name;
+	version_name = sessionStorage.version_name;
+	chapter_name = sessionStorage.chapter_name;
+	type = sessionStorage.type;
+	typeEnglish = sessionStorage.typeEnglish;
+
 	$("#versions_h").html(version_name + ' - ' + textbook_name + ' - ' + chapter_name);
 
 	//	alert(textbook_id+version_id+chapter_id+typeStr+textbook_name+version_name+chapter_name);
@@ -61,7 +63,7 @@ $(function() {
 					var thisWord = data.result[0];
 					//赋值当前单词题目的ID
 					thiswordId = thisWord.id;
-					
+
 					$("#thisWord").html(`<sub id="soundmark">${thisWord.phonogram}</sub> ${thisWord.word_name}`);
 
 					var answerHTML = `<h3>${thisWord.word_mean}</h3>
@@ -78,7 +80,7 @@ $(function() {
 						$("#audioplay").attr("src", audioplaySrc);
 					})
 
-					fnclickBtns();
+					//					fnclickBtns();
 					//保存在学课程信息
 					fnsavecourse();
 
@@ -87,17 +89,19 @@ $(function() {
 					window.location = 'alternativeVersion.html';
 				} else if(data.result == 2) {
 					alert("记忆结束，下面开始单词强化！");
-					window.location = 'alternativeVersion.html';
+					window.location = 'word_strengthen_memory.html';
 				}
 			}
 		});
-		
+
 		return false;
 
 	}
 
 	function fnshowcountDown() {
 		var num = 5;
+		$("#countDownTime").html(num);
+		
 		$("#countDown").show();
 		$("#answer").hide();
 		timer = setInterval(function() {
@@ -113,79 +117,83 @@ $(function() {
 
 	}
 	//点击认识或不认识
-	function fnclickBtns() {
-		$("#theFirstTime>button").on('click', function() {
-			var index = $(this).index();
-			if(index == 0) {
-				$(".btns").hide();
-				$("#secondTime").show();
-				//关闭定时器直接显示翻译内容
-				clearInterval(timer);
-				$("#countDown").hide();
-				$("#answer").show();
-				//点击是或否
-				$("#secondTime>button").on("click", function(ev) {
-					var ev = ev || window.event;
-					ev.cancelBubble = true;
-					
-					var index = $(this).index();
-					if(index == 0) { //是
-						//关闭定时器直接显示翻译内容
-						clearInterval(timer);
-						$("#countDown").hide();
-						$("#answer").show();
-						//进入下一个单词
-						fnnextWords(1);
-					} else { //否
-						//关闭定时器直接显示翻译内容
-						clearInterval(timer);
-						$("#countDown").hide();
-						$("#answer").show();
-						
-						fnshowstrengthenMemory();
-					}
-				})
-			} else {
-				//关闭定时器直接显示翻译内容
-				clearInterval(timer);
-				$("#countDown").hide();
-				$("#answer").show();
-				
-				fnshowstrengthenMemory();
-			}
-		})
-	}
+	//	function fnclickBtns() {
+	$("#theFirstTime>button").on('click', function(ev) {
+		//			ev.cancelBubble = true;
+		ev.stopPropagation();
+
+		var index = $(this).index();
+		if(index == 0) {
+			$(".btns").hide();
+			$("#secondTime").show();
+			//关闭定时器直接显示翻译内容
+			clearInterval(timer);
+			$("#countDown").hide();
+			$("#answer").show();
+
+		} else {
+			//关闭定时器直接显示翻译内容
+			clearInterval(timer);
+			$("#countDown").hide();
+			$("#answer").show();
+
+			fnshowstrengthenMemory();
+		}
+	})
+	//	}
+
+	//点击是或否
+	$("#secondTime>button").on("click", function(ev) {
+		ev.stopPropagation();
+
+		var index = $(this).index();
+		if(index == 0) { //是
+			clearInterval(timer);
+			$("#countDown").hide();
+			$("#answer").show();
+			//进入下一个单词
+			fnnextWords(1);
+		} else { //否
+			clearInterval(timer);
+			$("#countDown").hide();
+			$("#answer").show();
+
+			fnshowstrengthenMemory();
+		}
+	})
 
 	function fnshowstrengthenMemory() {
 		$(".btns").hide();
 		$("#strengthenMemory").show();
 		//点击两次 》 一次 》 下一步
-		var num = 2;
-		$("#strengthenMemory>button").on("click", function(ev) {
-			var ev = ev || window.event;
-			ev.cancelBubble = true;
-			num--;
-			if(num == 1) {
-				$("#ent_h").html(`${num}<sub>（跟着读两遍）</sub>`);
-				$("#audioplay").attr("src", audioplaySrc);
-			} else if(num == 0) {
-				$("#ent_h").addClass("ent_h").html(`<sub>（下一个）</sub>`);
-				$("#audioplay").attr("src", audioplaySrc);
-			}else if(num < 0){
-				//进入下一个单词
-				fnnextWords(2);
-			}
-		})
+		num = 2;
+		$("#ent_h").removeClass("ent_h").html(`${num}<sub>（跟着读两遍）</sub>`);
 	}
 	
-	function fnnextWords(thisstate){
-		alert(1);
+	$("#strengthenMemory>button").on("click", function(ev) {
+		var ev = ev || window.event;
+		ev.cancelBubble = true;
+		num--;
+		if(num == 1) {
+			$("#ent_h").html(`${num}<sub>（跟着读两遍）</sub>`);
+			$("#audioplay").attr("src", audioplaySrc);
+		} else if(num == 0) {
+			$("#ent_h").addClass("ent_h").html(`<sub>（下一个）</sub>`);
+			$("#audioplay").attr("src", audioplaySrc);
+		} else if(num < 0) {
+			//进入下一个单词
+			fnnextWords(2);
+		}
+	})
+
+	function fnnextWords(thisstate) {
+		//		alert(1);
 		//显示认识或不认识
 		$(".btns").hide();
 		$("#theFirstTime").show();
 		//显示倒计时
 		fnshowcountDown();
-		
+
 		$.ajax({
 			type: "POST",
 			url: thisUrl + '/Areas/Api/Interface.ashx',
@@ -202,6 +210,9 @@ $(function() {
 				//				alert(data.result.length);
 				if(data.result.length >= 1) {
 					var thisWord = data.result[0];
+					//赋值当前单词题目的ID
+					thiswordId = thisWord.id;
+
 					$("#thisWord").html(`<sub id="soundmark">${thisWord.phonogram}</sub> ${thisWord.word_name}`);
 
 					var answerHTML = `<h3>${thisWord.word_mean}</h3>
@@ -218,7 +229,7 @@ $(function() {
 						$("#audioplay").attr("src", audioplaySrc);
 					})
 
-					fnclickBtns();
+					//					fnclickBtns();
 					//保存在学课程信息
 					fnsavecourse();
 
@@ -227,21 +238,21 @@ $(function() {
 					window.location = 'alternativeVersion.html';
 				} else if(data.result == 2) {
 					alert("记忆结束，下面开始单词强化！");
-					window.location = 'alternativeVersion.html';
+					window.location = 'word_strengthen_memory.html';
 				}
 			}
-			
+
 		});
-		
+
 		return false;
 	}
 
 	$("#exit").on("click", function() {
 		this.href = 'alternativeVersion.html';
 	})
-	
+
 	//保存在学课程信息
-	function fnsavecourse(){
+	function fnsavecourse() {
 		$.ajax({
 			type: "POST",
 			url: thisUrl2 + '/Areas/Api/index.ashx',
@@ -253,7 +264,7 @@ $(function() {
 				textbook_id: textbook_id
 			},
 			success: function(data) {
-//				alert(data.msg);
+				//				alert(data.msg);
 			}
 		});
 	}
--- js/word_strengthen_memory.js
+++ js/word_strengthen_memory.js
@@ -1,26 +1,37 @@
-$(function(){
+$(function() {
 	//当前选择的版本ID，教材ID ,选择的章节
 	var textbook_id, chapter_id;
 	//当前语音文件播放路径
 	var audioplaySrc;
 	//该单元下所有单词
 	var wordsArr;
-	
-	$.getUrlParam = function(name) {
-		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
-		var r = window.location.search.substr(1).match(reg);
-		if(r != null) return decodeURI(r[2]);
-		return null;
-	};
-	textbook_id = $.getUrlParam('textbook_id');
-	chapter_id = $.getUrlParam('chapter_id');
-	textbook_name = $.getUrlParam('textbook_name');
-	version_name = $.getUrlParam('version_name');
-	chapter_name = $.getUrlParam('chapter_name');
-	
+	//当前语音播放路径
+	var audioplaySrc;
+
+	var num = 0;
+	//设定点击enter按钮的计数
+	var numEnt = 2;
+	//本章单词的数量
+	var wordArrlength;
+
+	//	$.getUrlParam = function(name) {
+	//		var reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)");
+	//		var r = window.location.search.substr(1).match(reg);
+	//		if(r != null) return decodeURI(r[2]);
+	//		return null;
+	//	};
+	textbook_id = sessionStorage.textbook_id;
+	version_id = sessionStorage.version_id;
+	chapter_id = sessionStorage.chapter_id;
+	typeStr = sessionStorage.typeStr;
+	textbook_name = sessionStorage.textbook_name;
+	version_name = sessionStorage.version_name;
+	chapter_name = sessionStorage.chapter_name;
+	type = sessionStorage.type;
+
 	fnGetAllTheWords();
-	
-	function fnGetAllTheWords(){
+
+	function fnGetAllTheWords() {
 		$.ajax({
 			type: "POST",
 			url: thisUrl + '/Areas/Api/Interface.ashx',
@@ -31,54 +42,78 @@ $(function(){
 			},
 			success: function(data) {
 				wordsArr = data.result;
-				
-				
+				wordArrlength = wordsArr.length;
+
+				fnshowthisWord(wordsArr[num]);
 			}
 		});
 	}
-	
-	function fnshowthisWord(wordObj){
+	//载入对应单词
+	function fnshowthisWord(wordObj) {
+		$("#thisStudy").html('进度：'+(num+1)+'/'+wordArrlength);
+		
+		$("#wordinput").val("").attr('disabled',false);
+		$("#wordinput")[0].focus();
+		$("#answer").hide();
+		$("#status").hide();
+		
+		$("#translate").html(wordObj.word_mean);
+		$("#answer").html(wordObj.word_name);
+
+		//设置播放路径，绑定听语音事件
+		audioplaySrc = thisUrl2 + wordObj.word_url;
+		$("#audioplay").attr("src", audioplaySrc);
+
+		numEnt = 2;
+	}
+	//听语音按钮
+	$("#voice").on("click", function() {
+		$("#audioplay").attr("src", audioplaySrc);
+	})
+	//enter按钮
+	$("#enter").on("click", function() {
+		if(numEnt == 2) {
+			numEnt--;
+			
+			$("#answer").show();
+			$("#status").show();
+
+			//输入的内容和正确答案进行对比
+			var inputVal = $("#wordinput").val();
+			if(inputVal == $("#answer").html()) {
+				$("#audioplay").attr("src", audioplaySrc);
+				
+				$("#answer").css("color", "#57b3ff");
+				$("#status").attr("class", "status correct");
+				$("#wordinput").attr("disabled", true);
+				
+				numEnt--;
+			} else {
+				$("#answer").css("color", "#ff1919");
+				$("#status").attr("class", "status");
+				$("#wordinput").attr("disabled", true);
+								
+			}
+		}else if(numEnt == 1){
+			$("#wordinput").val("").attr('disabled', false);
+			$("#wordinput")[0].focus();
+			
+			numEnt = 2;
+			
+		}else if(numEnt <= 0){
+			fnnextWord();
+		}
+	})
+
+	function fnnextWord() {
+		num++;
+		if(num < wordArrlength){
+			fnshowthisWord(wordsArr[num]);
+		}else if(num >= wordArrlength){
+			alert("记忆强化完成，下面开始测试");
+			window.location="word_simulationTest.html";
+		}
 		
 	}
-	
-	
+
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
