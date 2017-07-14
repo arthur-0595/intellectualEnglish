838dee05c759144e22e42fe5b7518b0fea231af8
尹彦 <751410306@qq.com>
2017-07-14 18:19:55 +0800

    例句听力还差一个测试

--- js/sentence_listen.js
+++ js/sentence_listen.js
@@ -15,8 +15,10 @@ $(function() {
 	var audioplaySrc;
 	//当前例句大类的类型
 	var thistype = 1; //1听力2翻译3默写
-	//当前听力的句子变量,由句子的每个单个项组成的数组
-	var thisSentence, thisSentenceArr;
+	//当前听力的句子变量,由句子的每个单个项组成的数组,顺序没打乱时的数组
+	var thisSentence, thisSentenceArr , sentenceInTheRightOrderArr;
+	//当前正确或者错误的状态值
+	var typeNum = 2;
 
 	textbook_id = sessionStorage.textbook_id;
 	version_id = sessionStorage.version_id;
@@ -44,6 +46,18 @@ $(function() {
 		$("#audioplay").attr("src", audioplaySrc);
 	});
 
+	$("#clear").on("click", function() {
+		$("span.ans_word").html('').attr('class', 'ans_null');
+		$("li.actived").attr('class', '')
+			.css('cursor', 'pointer');
+		fnclickItems();
+	})
+
+	$("#reset").unbind().on("click", function() {
+		fnclickresetBtn();
+		$("#thisSentence_con").show();
+	})
+
 	function fnUpdatesentence() {
 		$.ajax({
 			type: "POST",
@@ -62,14 +76,210 @@ $(function() {
 				var processorSentence = fnprocessor(thisSentence.sentence);
 				//将句子切割成数组
 				thisSentenceArr = processorSentence.split(' ');
+				sentenceInTheRightOrderArr = processorSentence.split(' ');
 				console.log(thisSentenceArr);
 				//自动播放语音文件
 				audioplaySrc = thisUrl2 + thisSentence.sentence_url;
 				$("#audioplay").attr("src", audioplaySrc);
 				//获取到数据之后更新对应的句子相关内容
-				fnUpdateAll(thisSentence, thisSentenceArr);
+				fnUpdateAll(thisSentence, thisSentenceArr ,sentenceInTheRightOrderArr);
+
 			}
 		});
+	}
+
+	function fnUpdateAll(thisSentence_, thisSentenceArr_ , sentenceInTheRightOrderArr_) {
+		typeNum = 2;
+
+		$("#clear").show();
+		$("#reset").hide();
+
+		$("#thisSentence_con").hide().html(thisSentence_.sentence);
+		$("#interpret").html(thisSentence_.sentence_mean);
+		var re = /\,|\.|\!|\?/g;
+		//趁数组还没有进行随机打乱的时候，填充上面答案列表的内容
+		var answerArr_html = '';
+		$.each(sentenceInTheRightOrderArr_, function(index, element) {
+			if(!re.test(element)) {
+				answerArr_html += `<span class="ans_null" id="${element}"></span>`;
+			} else {
+				answerArr_html += `<span class="punctuation">${element}</span>`;
+			}
+		});
+		$("#answerArr").attr('class', 'answerArr').html(answerArr_html);
+		//打乱数组的内容
+		thisSentenceArr_.sort(function() {
+			return(0.5 - Math.random());
+		})
+		console.log(thisSentenceArr_);
+
+		//把处理过后的数组的每一项填到下面的选项中
+		var items_html = '';
+		$.each(thisSentenceArr_, function(index, element) {
+			if(!re.test(element)) {
+				items_html += `<li id="${element}">${element}</li>`;
+			}
+		});
+		$("#items").show().html(items_html);
+		//答案选项的点击事件
+		fnclickItems();
+
+		$("#enter").unbind().on("click", function() {
+			fncontrast();
+		})
+	}
+
+	function fncontrast() {
+		typeNum--;
+
+		if(typeNum == 1) {
+			//首先获取下面正确选项的答案组成字符串
+			var botString = '';
+			botString = fnprocessor2(thisSentence.sentence);
+			console.log(botString);
+			//获取上面回答的选项内容组成字符串
+			var topString = '';
+			$.each($("span.ans_word"), function(index, element) {
+				topString += element.innerHTML;
+			})
+			console.log(topString);
+
+			$("#thisSentence_con").show();
+			$("#clear").hide();
+			$("#reset").show();
+			$("#items").html(thisSentence.sentence_mean);
+			if(botString === topString) { //校验正确时
+				$("#answerArr").attr("class", 'answerArr correct')
+					.find("span").css("color", '#57b3ff');
+				//清除重组按钮的点击事件	
+				$("#reset").attr('class', 'reset')
+					.unbind()
+					.css("cursor", 'default');
+				//给enter按钮改变相应的样式，并绑定点击事件
+				$("#enter").css({
+					backgroundColor: '#ff7b57',
+					cursor: 'pointer'
+				}).unbind().on("click", function() {
+					fncontrast();
+				})
+				
+				$("span.ans_word").css('color' , '#57b3ff');
+
+			} else { //校验错误时，改变字体颜色，并标注对应的符号
+				$("#answerArr").attr("class", 'answerArr error')
+					.find("span").css("color", '#ff1919');
+				//给重组按钮绑定对应的点击事件	
+				$("#reset").attr('class', 'reset err')
+					.unbind()
+					.css("cursor", 'pointer')
+					.on('click', function() {
+						fnclickresetBtn();
+						$("#thisSentence_con").show();
+					});
+				//改变enter按钮的状态和颜色，取消其点击事件
+				$("#enter").css({
+					backgroundColor: '#999',
+					cursor: 'default',
+					boxShadow: 'none'
+				}).unbind();
+				
+				fnestimateType();
+
+				typeNum = 2;
+			}
+		} else if(typeNum <= 0) {
+			fnupdateNext();
+		}
+
+	}
+	//更新下一个例句的相关内容
+	function fnupdateNext() {
+		var sentenceType = 1;
+		$.ajax({
+			type: "POST",
+			url: thisUrl + "/Areas/api/Interface.ashx",
+			dataType: "json",
+			data: {
+				method: 'getnextSentence',
+				id: thisSentence.id,
+				neworold_word: sentenceType,
+				user_id: username,
+				unit_id: chapter_id,
+				type: thistype
+			},
+			success: function(data) {
+				console.log(JSON.stringify(data));
+				thisSentence = data[0];
+
+				var processorSentence = fnprocessor(thisSentence.sentence);
+				//将句子切割成数组
+				thisSentenceArr = processorSentence.split(' ');
+				sentenceInTheRightOrderArr = processorSentence.split(' ');
+				console.log(thisSentenceArr);
+				//自动播放语音文件
+				audioplaySrc = thisUrl2 + thisSentence.sentence_url;
+				$("#audioplay").attr("src", audioplaySrc);
+				//获取到数据之后更新对应的句子相关内容
+				fnUpdateAll(thisSentence, thisSentenceArr ,sentenceInTheRightOrderArr);
+			}
+		});
+	}
+	
+	function fnestimateType(){
+		$.each( $("span.ans_word") , function(index , element) {
+			if(element.innerHTML == element.id){
+				$(element).css('color' , '#57b3ff');
+			}else{
+				$(element).css('color' , '#ff1919');
+			}
+		});
+		
+	}
+	
+	function fnrecallEvent(){
+		$("span.ans_word").unbind()
+			.on('click' , function(){
+				var topVal = this.innerHTML;
+				
+				$(this).html('').attr('class' , 'ans_null');
+				
+				$.each($("#items>li"), function(index , element) {
+					if(element.id == topVal){
+						$(element).attr('class' , '')
+							.css("cursor", 'pointer');
+					}
+				});
+				
+				fnclickItems();
+			})
+	}
+
+	function fnclickItems() {
+		$("#items>li").unbind().on("click", function() {
+			var thisVal = $(this).html();
+			$(".ans_null").eq(0).html(thisVal).attr('class', "ans_word");
+			$(this).attr('class', 'actived').css("cursor", 'default').unbind();
+
+			$("#items>li").on("selectstart", function() {
+				return false;
+			})
+			
+			fnrecallEvent();
+		})
+	}
+
+	function fnclickresetBtn() {
+		$("#enter").css({
+				backgroundColor: '#ff7b57',
+				cursor: 'pointer'
+			})
+			.unbind()
+			.on("click", function() {
+				fncontrast();
+			})
+
+		$("#audioplay").attr("src", audioplaySrc);
+		fnUpdateAll(thisSentence, thisSentenceArr ,sentenceInTheRightOrderArr);
 	}
 
 	function fnprocessor(sentence_) {
@@ -80,33 +290,13 @@ $(function() {
 		return sentence_;
 	}
 
-	function fnUpdateAll(thisSentence_, thisSentenceArr_) {
-		$("#thisSentence_con").html(thisSentence_.sentence);
-		$("#interpret").html(thisSentence_.sentence_mean);
-		var re = /^\,|\.|\!|\?$/g;
-
-		thisSentenceArr_.sort(function() {
-			return(0.5 - Math.random());
-		})
-		alert(thisSentenceArr_);
-
-		//把处理过后的数组的每一项填到下面的选项中
-		var items_html = '';
-		$.each(thisSentenceArr_, function(index, element) {
-			if(!re.test(element)) {
-				items_html += `<li>${element}</li>`;
-			}
-		});
-		$("#items").show().html(items_html);
-
-		//		$.each($("#answerArr>span"), function(index , element) {
-		//			if( re.test(element.innerHTML) ){
-		//				$(element).attr('class' , 'punctuation');
-		//			}
-		//		});
-		//当回答错误时，改变字体颜色，并标注对应的符号
-		//		$("#answerArr").attr("class" , 'answerArr error')
-		//					.find("span").css("color" , '#ff1919');
+	function fnprocessor2(sentence_) {
+		sentence_ = sentence_.replace(/\,+/g, '');
+		sentence_ = sentence_.replace(/\.+/g, '');
+		sentence_ = sentence_.replace(/\?+/g, '');
+		sentence_ = sentence_.replace(/\!+/g, '');
+		sentence_ = sentence_.replace(/\s+/g, '');
+		return sentence_;
 	}
 
 })
